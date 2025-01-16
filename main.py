0b683c57556efd4b6a8666e2ef4bfd62d683dbc5
Prachi1416 <prachidongre0404@gmail.com>
2025-01-16 16:05:11 +0530

    Addded Calculator fuctionalities in the application.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from functionalities import addition, subtraction, multiplication, division
+
 print("Hello World")
+
+print(addition(10,20))
+print(subtraction(10,20))
+print(multiplication(10,20))
+print(division(10,20))
