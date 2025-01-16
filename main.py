003354c89e625c0a5624d3b3b3da2ffdbfeadb5c
Prachi1416 <prachidongre0404@gmail.com>
2025-01-16 16:13:29 +0530

    Implemented menu driven code.

--- main.py
+++ main.py
@@ -2,7 +2,32 @@ from functionalities import addition, subtraction, multiplication, division
 
 print("Hello World")
 
-print(addition(10,20))
-print(subtraction(10,20))
-print(multiplication(10,20))
-print(division(10,20))
+while True:
+    ch = int(input("1.addition\n2.subtraction\n3.multiplication\n4.division\nEnter your choice: "))
+    match ch :
+        case 1 :
+            num1 = int(input("Enter First Number: "))
+            num2 = int(input("Enter Second Number: "))
+            print(addition(num1,num2))
+
+        case 2 :
+            num1 = int(input("Enter First Number: "))
+            num2 = int(input("Enter Second Number: "))
+            print(subtraction(num1,num2))
+
+        case 3 :
+            num1 = int(input("Enter First Number: "))
+            num2 = int(input("Enter Second Number: "))
+            print(multiplication(num1,num2))
+
+        case 4 :
+            num1 = int(input("Enter First Number: "))
+            num2 = int(input("Enter Second Number: "))
+            print(division(num1,num2))
+
+        case 0:
+            break
+
+        case _:
+            print("Enter valid choice")
+
